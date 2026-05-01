aa874939b4ab1b617a4e794329340ded74d6d508
Franz Zechner <78723313+fu-cap@users.noreply.github.com>
2026-05-01 11:48:46 +0200

    basic routing added

--- tour-planner-frontend/src/app/app.routes.ts
+++ tour-planner-frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home.component/home.component';
+import { LoginComponent } from './pages/login.component/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+];
