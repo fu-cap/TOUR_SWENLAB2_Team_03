62aea0823c6c7f89b394328649bfe5e01a630a87
Franz Zechner <78723313+fu-cap@users.noreply.github.com>
2026-05-12 16:02:32 +0200

    added create action button

--- tour-planner-frontend/src/app/components/navbar/navbar.ts
+++ tour-planner-frontend/src/app/components/navbar/navbar.ts
@@ -1,7 +1,7 @@
 import { Component, Output, EventEmitter, signal } from '@angular/core';
 import { Navbutton } from '@/components/navbutton/navbutton';
 
-export type AppState = 'overview' | 'details' | 'edit' | 'logs' | 'user';
+export type AppState = 'overview' | 'details' | 'edit' | 'logs' | 'user' | 'create';
 
 interface NavItem {
   icon: string;
@@ -20,12 +20,19 @@ export class Navbar {
 
   activeState = signal<AppState>('overview');
 
-  navItems: NavItem[] = [
+  mainNavItems: NavItem[] = [
     {icon: 'list', label: 'Overview', state: 'overview'},
     {icon: 'dashboard', label: 'Details', state: 'details'},
-    {icon: 'dashboard_2_edit', label: 'Edit', state: 'edit'},
+    {icon: 'edit', label: 'Edit', state: 'edit'},
     {icon: 'comment', label: 'Logs', state: 'logs'},
-    {icon: 'account_circle', label: 'Account', state: 'user'}
+  ];
+
+  actionNavItems: NavItem[] = [
+    {icon: 'add', label: 'Create', state: 'create'},
+  ];
+
+  userItem: NavItem[] = [
+    {icon: 'account_circle', label: 'Account', state: 'user'},
   ];
 
   onStateChange(state: AppState): void {
