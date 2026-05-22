b46d7fccaea9dc417c730ef09269304dbd683cd1
Franz Zechner <78723313+fu-cap@users.noreply.github.com>
2026-05-22 21:19:38 +0200

    api env variable implementation and localhost error fix in Programm.cs

--- tour-planner-backend/TourPlanner.API/Program.cs
+++ tour-planner-backend/TourPlanner.API/Program.cs
@@ -30,7 +30,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AngularFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:4200")
+        policy.WithOrigins("http://localhost:4200", "http://localhost")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
